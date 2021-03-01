676792574784a7e7dca8be4462519e7e7136d3ac
Roseanne Phillips <rphillips.rp@gmail.com>
2021-03-01 11:31:52 -0500

    installed nodemon

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
-//initialize express
+//initialize express create server
 const app = express();
 
-//set static folder to public to join needed routes
+//set static folder to public 
 app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
 
+//tell server to listen
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
