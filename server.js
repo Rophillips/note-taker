b64288c649c1abe810fe42a59b8f661e93fe50d7
Roseanne Phillips <rphillips.rp@gmail.com>
2021-03-01 22:36:59 -0500

    adding api routes for notes

--- server.js
+++ server.js
@@ -8,14 +8,29 @@ const app = express();
 
 //set static folder to public 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 //tell server to listen
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
- //set up to get route
- app.get("/api/notes", function(req, res){
- //read db.json file and save as json
-     res.json(notes);
- });
+
+//set up to get API notes route 
+app.get("api/notes", function (req, res) {
+    //  //read db.json file and save as json
+    res.json(notes);
+});
+
+
+app.post("/api/notes", function(req,res) {
+    let newNote = req.body;
+    notes.push(newNote);
+    updateDb();
+    return console.log("Added new note: "+newNote.title);
+})
+
+
+
+
